842a6957e64c3f9bc0571142ccc7a0adf5c95ff2
Bruno <bruno.mcamps@outlook.com>
2018-08-31 08:16:08 -0300

    Initial Commit

---
+++ Automato/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Automato
+//
+//  Created by Bruno Campos on 8/31/18.
+//  Copyright © 2018 Bruno Campos. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
